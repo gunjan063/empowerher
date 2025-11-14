978c0d9db6e2b61ee20dc90e8fa173906a63f7a0
gunjan063 <gunjanbirla67@gmail.com>
2025-11-14 07:02:54 -0800

    coded in another branch

--- day_6_assignment/ques_11/index.js
+++ day_6_assignment/ques_11/index.js
@@ -14,10 +14,11 @@ function isprime(num) {
         return false
     }
 }
+
 console.log(isprime(num));
 
-function evenOdd(num) {
-    return num % 2 === 0;
-}
-
-console.log(evenOdd(4))
+if (num%2==0){
+    console.log("even");
+ } else {
+    console.log("odd");
+ }
