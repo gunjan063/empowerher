eeb579ce371521ea99035ff426bfa0072740f3ae
gunjan063 <gunjanbirla67@gmail.com>
2025-11-14 08:01:57 -0800

    signupcheckdone

--- day_6_assignment/ques_11/ques_12/index.js
+++ day_6_assignment/ques_11/ques_12/index.js
@@ -11,3 +11,22 @@ function signup(userName) {
 
 console.log(signup("gunjan"));  
 console.log(signup("newUser"));
+
+
+function login(userName, password) {
+    const users = ["gunjan", "tanya", "anaya", "sanyaa"];
+
+    if (!users.includes(userName)) {
+        return "User Not Found, Please Signup";
+    }
+
+    if (password !== "Emp@123") {
+        return "Wrong Password....";
+    }
+
+    return "Login Successful...";
+}
+
+console.log(login("gunjan", "Emp@123"));   
+console.log(login("gunjan", "wrong"));    
+console.log(login("unknown", "Emp@123"));  
