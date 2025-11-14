13f4d4736a5c74c00025d759e7c7015c95908d21
gunjan063 <gunjanbirla67@gmail.com>
2025-11-14 03:05:28 -0800

    first repo done

---
+++ day_6_assignment/ques_11/index.js
@@ -0,0 +1,17 @@
+
+let num=2;
+
+function isprime(num) {
+    let count=0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            count++
+        }   
+    }
+    if (count === 2) {
+        return true;
+    }   else{   
+        return false
+    }
+}
+console.log(isprime(num));
